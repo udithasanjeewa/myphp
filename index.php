901efb1617f713c74378c4aa77be737006ed31e8
udithasanjeewa <66052814+udithasanjeewa@users.noreply.github.com>
2020-05-28 05:32:38 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+echo "Hello Uditha Sanjeewa"
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<div class="jumbotron text-center">
+  <h1>My First PHP Page</h1>
+  <p>Resize this responsive page to see the effect!</p>
+</div>
+
+<div class="container">
+  <div class="row">
+    <div class="col-sm-4">
+      <h3>PHP</h3>
+      <p>Lorem ipsum dolor..</p>
+    </div>
+    <div class="col-sm-4">
+      <h3>JAVA</h3>
+      <p>Lorem ipsum dolor..</p>
+    </div>
+    <div class="col-sm-4">
+      <h3>CSS</h3>
+      <p>Lorem ipsum dolor..</p>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
